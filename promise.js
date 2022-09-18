6b0c383f58bd9cb71a6a081a0ca820a86107f21d
immsswd <i.mail@tuta.io>
2022-09-18 21:49:36 +0700

    masih memahami

--- promise.js
+++ promise.js
@@ -1,7 +1,7 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = new Promise((resolve, reject) => {});
 
 module.exports = {
   promiseOutput,
